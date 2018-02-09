ad7729cbabb97d268d1cdd665b90ca6c97aa055e
“agatarajewska <agatarajewska@gmail.com>
2018-02-09 19:33:55 +0100

    zadanie 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator myCalculator = new Calculator(20, 10);
+
+        int adding = myCalculator.add();
+        int substracting = myCalculator.substract();
+
+        if (adding == 30) {
+            System.out.println("adding test is ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if (substracting == 10) {
+            System.out.println("substracting test is ok");
+        } else {
+            System.out.println("Error!");
+
+        }
     }
+
+
 }
+
+
+
+
