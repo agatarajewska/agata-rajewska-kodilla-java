3d01f646e874f752050cfeb16fe916746a9d8cfc
“agatarajewska <agatarajewska@gmail.com>
2018-03-26 18:09:00 +0200

    zadanie 6.3

---  kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++  kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -5,10 +5,16 @@ import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 import com.kodilla.stream.person.People;
+import com.kodilla.stream.book.BookDirectory;
+import java.util.List;
+import java.util.stream.Collectors;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.book.Book;
+import java.util.Map;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+       /* ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
         System.out.println("Calculating expressions with lambdas");
         expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
@@ -24,16 +30,41 @@ public class StreamMain {
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
-        System.out.println(poemBeautifier.beautify("Ala", (text) -> text+"ABC" ));
+        System.out.println(poemBeautifier.beautify("Ala", (text) -> text + "ABC"));
         System.out.println(poemBeautifier.beautify("Ala", (text) -> text.toUpperCase()));
 
-        System.out.println(poemBeautifier.beautify("Ala", (text) -> "Prefix: "+text ));
+        System.out.println(poemBeautifier.beautify("Ala", (text) -> "Prefix: " + text));
         System.out.println(poemBeautifier.beautify("Ala", (text) -> text.replace('A', 'U')));
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
+
+
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println); */
+
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
+
+        System.out.println(theResultStringOfBooks);
+
+
+
+
+
+
+
+
+
     }
 
-
-
 }
+
---  kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++  kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -6,4 +6,5 @@ public class PoemBeautifier {
 
         return result;
     }
+
 }
